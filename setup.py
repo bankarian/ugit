6a2716fb6865211461fde3a5d0a0f2ab005d01bc
Beney Chen <beney-chen@qq.com>
2022-04-12 11:10:27 +0800

    object hashing

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-setup(name="ugit", version="1.0", packages=['ugit'], author='Benny Chen', entry_points={
-    'console_scripts': [
-        'ugit = ugit.cli:main'
-    ]
-})
+setup(name="ugit",
+      version="1.0",
+      packages=['ugit'],
+      author='Benny Chen',
+      entry_points={'console_scripts': ['ugit = ugit.cli:main']})
