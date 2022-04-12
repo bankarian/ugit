4ae771754032ee4bf2903280f2b9ee86707b59db
Beney Chen <beney-chen@qq.com>
2022-04-12 13:29:26 +0800

    add types to objects

--- ugit/data.py
+++ ugit/data.py
@@ -3,22 +3,31 @@ import hashlib
 
 GIT_DIR = '.ugit'
 
+
 def init():
     os.makedirs(GIT_DIR)
     os.makedirs(f"{GIT_DIR}/objects")
 
 
-def hash_object(data) -> str:
+def hash_object(data, type_='blob') -> str:
     '''
     Content-addressable storage, save data to a new file with name of hash(data), return object id.
     '''
-    oid = hashlib.sha1(data).hexdigest()
+    obj = type_.encode() + b'\x00' + data
+    oid = hashlib.sha1(obj).hexdigest()
     path = f"{GIT_DIR}/objects/{oid}"
-    
+
     with open(path, 'wb') as out:
-        out.write(data)
+        out.write(obj)
     return oid
 
-def get_object(oid: str):
+
+def get_object(oid: str, expected='blob'):
     with open(f"{GIT_DIR}/objects/{oid}", 'rb') as f:
-        return f.read()
+        obj = f.read()
+    type_, _, content = obj.partition(b'\x00')
+    type_ = type_.decode()
+
+    if expected is not None:
+        assert type_ == expected, f'Expected {expected}, got {type_}'
+    return content
