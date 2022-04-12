2b0e00156b33a17cba7310a35fe4c5ebbbd45126
Beney Chen <beney-chen@qq.com>
2022-04-12 13:06:22 +0800

    cat-file

--- ugit/data.py
+++ ugit/data.py
@@ -5,10 +5,20 @@ GIT_DIR = '.ugit'
 
 def init():
     os.makedirs(GIT_DIR)
+    os.makedirs(f"{GIT_DIR}/objects")
 
 
 def hash_object(data) -> str:
+    '''
+    Content-addressable storage, save data to a new file with name of hash(data), return object id.
+    '''
     oid = hashlib.sha1(data).hexdigest()
-    with open(f"{GIT_DIR}/objects/{oid}", 'wb') as out:
+    path = f"{GIT_DIR}/objects/{oid}"
+    
+    with open(path, 'wb') as out:
         out.write(data)
     return oid
+
+def get_object(oid: str):
+    with open(f"{GIT_DIR}/objects/{oid}", 'rb') as f:
+        return f.read()
