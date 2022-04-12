265bf3c9d5b46593ba26309f0967ef6c34b3f3a6
Beney Chen <beney-chen@qq.com>
2022-04-12 10:55:38 +0800

    init setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name="ugit", version="1.0", packages=['ugit'], author='Benny Chen', entry_points={
+    'console_scripts': [
+        'ugit = ugit.cli:main'
+    ]
+})
