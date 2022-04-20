afc927ebc455d84534bd2b97d0d160ca912e97db
Beney Chen <beney-chen@qq.com>
2022-04-20 10:07:47 +0800

    print diff of commit

--- ugit/diff.py
+++ ugit/diff.py
@@ -1,8 +1,14 @@
 """ Module that deals with computing differences between objects.
 """
 
+import subprocess
+import os
+
 from collections import defaultdict
+from tempfile import NamedTemporaryFile as Temp
 from typing import Iterable, Dict
+
+from . import data
 
 
 def compare_trees(*trees):
@@ -23,8 +29,40 @@ def diff_trees(t_from: Dict[str, str], t_to: Dict[str, str]):
     Take two trees, compares them and return all entries that
     have differenct OIDs.
     """
-    output = ''
-    for path, o_from, o_to, in compare_trees(t_from, t_to):
+    output = b''
+    for (
+        path,
+        o_from,
+        o_to,
+    ) in compare_trees(t_from, t_to):
         if o_from != o_to:
-            output += f'changed: {path}\n'
+            # output += f"changed: {path}\n"
+            output += diff_blobs(o_from, o_to, path)
+    return output
+
+
+def diff_blobs(o_from, o_to, path="blob"):
+    with Temp(delete=False) as f_from, Temp(delete=False) as f_to:
+        for oid, f in ((o_from, f_from), (o_to, f_to)):
+            if oid:
+                f.write(data.get_object(oid))
+                f.flush()
+        with subprocess.Popen(
+            [
+                "diff",
+                "--unified",
+                "--show-c-function",
+                "--label",
+                f"a/{path}",
+                f_from.name,
+                "--label",
+                f"b/{path}",
+                f_to.name,
+            ],
+            stdout=subprocess.PIPE,
+        ) as proc:
+            output, _ = proc.communicate()
+    # Mannually remove the tempfiles due to Windows file permission issues
+    os.remove(f_from.name)
+    os.remove(f_to.name)
     return output
